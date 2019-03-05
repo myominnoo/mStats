b6c4e2e34a4c402cacd13ef8dd44dd4858f400cd
Myo Minn Oo <dr.myominnoo@gmail.com>
2019-03-06 00:30:04 +0700

    mStats Version 1.04.01.03

--- R/data_management.R
+++ R/data_management.R
@@ -1,10 +1,81 @@
+#' @title Remove Duplicated Elements
+#' @description
+#' \code{dup.rm} determines which elements of a vector are duplicates and removes them.
+#' @param x a numeric object
+#' @param na.rm A logical value to specify missing values
+#' @details
+#' This becomes handy when a return value with duplicates removed is need. It checks if
+#' any elements of vector are duplicated and remove them at the same time.
+#' @seealso \code{\link{label}}
+#' @keywords duplicate, remove duplicate
+#' @author Myo Minn Oo (Email: \email{dr.myominnoo@@gmail.com} |
+#' Website: \url{https://myominnoo.github.io/})
+#' @examples
+#' ## Example 1
+#' a <- rep(c("A", "B", "C"), c(10, 20, 30))
+#' a
+#' dup.rm(a)
+#'
+#' ## Example 2
+#' b <- rep(c("A", "B", "C", NA), c(10, 20, 30, 10))
+#' b
+#' dup.rm(b)
+#' dup.rm(b, na.rm = TRUE)
 
+#' @export
 dup.rm <- function(x, na.rm = FALSE) {
   x <- x[duplicated(x) == FALSE]
   if (na.rm) x <- x[!is.na(x)]
   return(x)
 }
 
+
+
+
+#' @title Label levels of a categorical variable (Value Label)
+#' @description
+#' \code{label} function is handy to quickly change levels of either a factor, number,
+#' logical or character vector.
+#' @param x A vector
+#' @param labels specify a character vector that correspond to the levels
+#' @details
+#' This function allows to quickly change vector into factor with defined levels or even
+#' levels of a factor into new ones.
+#' @seealso \code{\link{dup.rm}}
+#' @keywords value label, label levels
+#' @author Myo Minn Oo (Email: \email{dr.myominnoo@@gmail.com} |
+#' Website: \url{https://myominnoo.github.io/})
+#' @examples
+#' data(infert)
+#' str(infert)
+#'
+#' ## Example 1: Numeric vector
+#' itab(case, data = infert)
+#' case <- label(infert$case, c("No", "Yes"))
+#' itab(case)
+#'
+#' ## Example 2: factor
+#' itab(education, data = infert)
+#' edu <- label(infert$education, c("low", "middle", "high"))
+#' itab(edu)
+#'
+#' ## Example 3: character
+#' sex <- rep(c("Male", "Female", NA), c(30, 60, 10))
+#' str(sex)
+#' itab(sex)
+#' sex1 <- label(sex, c('f', 'm'))
+#' str(sex1)
+#' itab(sex1)
+#'
+#' ## Example 4: Logical
+#' s <- sex == "Male"
+#' str(s)
+#' itab(s)
+#' s1 <- label(s, c("male", "female"))
+#' str(s1)
+#' itab(s1)
+
+#' @export
 label <- function(x, labels)
 {
   if (is.numeric(x)) {
--- R/isum.R
+++ R/isum.R
@@ -1,70 +1,70 @@
 #' @title Number Summary, Correlation, Grouped and Stratified Summary
 #' @description
 #' \code{isum} generates tables that display univariate, bivariate and stratified
-#' summaries. 
+#' summaries.
 #' @param x a numeric object
 #' @param y a numeric or factor object
 #' @param by a factor object
-#' @param data an optional data frame 
+#' @param data an optional data frame
 #' @param rnd specify rounding of numbers
 #' @param na.rm A logical value to specify missing values, <NA> in the table
 #' @param plot.show display bar plots of the data
-#' @details 
-#' Exploring data before jumping into complex analysis is always a necessity. 
+#' @details
+#' Exploring data before jumping into complex analysis is always a necessity.
 #' The first step of an analysis should be to summarize and display data.
 #' By doing so, invaluable insight can be gained through the familiarity with the data.
-#' 
-#' \code{isum} allows to summarize the distribution of data taken by a single variable, 
+#'
+#' \code{isum} allows to summarize the distribution of data taken by a single variable,
 #' the association between two numeric variables as well as between a numeric variable
-#' and a factor variable, and finally these associations stratified by a third factor 
-#' variable. 
-#' 
-#' This function also generate different data visualization for better exploration by 
-#' using \code{iboxplot}. The plot display can be switched off by setting 
+#' and a factor variable, and finally these associations stratified by a third factor
+#' variable.
+#'
+#' This function also generate different data visualization for better exploration by
+#' using \code{iboxplot}. The plot display can be switched off by setting
 #' \code{plot.show = FALSE}.
-#' 
+#'
 #' \strong{References:}
 #' \enumerate{
-#'   \item Essential Medical Statistics, Betty R. Kirkwood & Jonathan A.C. Sterne, 
+#'   \item Essential Medical Statistics, Betty R. Kirkwood & Jonathan A.C. Sterne,
 #'   Second Edition. Chapter 3
-#'   \item An Introduction to MEdical Statistics, Martin Bland, Thrid Edition, 
+#'   \item An Introduction to MEdical Statistics, Martin Bland, Thrid Edition,
 #'   Chapter 4
 #' }
-#' 
+#'
 #' @import ggplot2
-#' @seealso \code{\link{itab}}, \code{\link{ibarplot}}, \code{\link{strate}}, 
+#' @seealso \code{\link{itab}}, \code{\link{ibarplot}}, \code{\link{strate}},
 #' @keywords distribution, number summary, correlation
 #' @author Myo Minn Oo (Email: \email{dr.myominnoo@@gmail.com} |
 #' Website: \url{https://myominnoo.github.io/})
 #' @examples
 #' data(infert)
 #' str(infert)
-#' 
-#' ## univariate analysis 
+#'
+#' ## univariate analysis
 #' isum(infert$age)
 #' isum(pooled.stratum, data = infert)
-#' 
+#'
 #' ## bivariate: numeric x factor
 #' isum(age, education, data = infert)
 #' isum(parity, education, data = infert)
 #' isum(pooled.stratum, education, data = infert)
 #' isum(infert$parity, factor(infert$case))
-#' 
-#' ## bivariate: numeric x numeric 
+#'
+#' ## bivariate: numeric x numeric
 #' isum(age, parity, data = infert)
 #' isum(age, stratum, data = infert)
 #' isum(age, pooled.stratum, data = infert)
-#' 
-#' ## stratified: numeric x factor | factor 
+#'
+#' ## stratified: numeric x factor | factor
 #' infert$case <- factor(infert$case)
 #' isum(age, education, case, data = infert)
-#' 
-#' ## stratified: numeric x numeric | factor 
+#'
+#' ## stratified: numeric x numeric | factor
 #' isum(age, parity, education, data = infert)
 #' isum(age, stratum, education, data = infert)
 
-#' @export 
-isum <- function(x, y = NULL, by = NULL, data = NULL, rnd = 1, na.rm = TRUE, 
+#' @export
+isum <- function(x, y = NULL, by = NULL, data = NULL, rnd = 1, na.rm = TRUE,
                  plot.show = TRUE)
 {
   if (!is.null(data)) {
@@ -72,7 +72,7 @@ isum <- function(x, y = NULL, by = NULL, data = NULL, rnd = 1, na.rm = TRUE,
     x <- eval(substitute(x), data)
     y <- eval(substitute(y), data)
     by <- eval(substitute(by), data)
-    
+
     x.name <- arguments$x
     y.name <- arguments$y
     by.name <- arguments$by
@@ -81,13 +81,13 @@ isum <- function(x, y = NULL, by = NULL, data = NULL, rnd = 1, na.rm = TRUE,
     if (!is.null(y)) y.name <- deparse(substitute(y))
     if (!is.null(by)) by.name <- deparse(substitute(by))
   }
-  
+
   if (!is.numeric(x)) {
     stop("x must be a numeric object.")
   }
-  
+
   type <- ifelse(is.null(y), "uni", ifelse(is.null(by), "bi", "strata"))
-  
+
   res <- switch(
     type,
     uni = {
@@ -95,7 +95,7 @@ isum <- function(x, y = NULL, by = NULL, data = NULL, rnd = 1, na.rm = TRUE,
       row.names(df) <- x.name
       cat(paste0('\nSummary: ', x.name, '\n\n'))
       uni <- df
-    }, 
+    },
     bi = {
       if (is.factor(y) | is.character(y) | is.logical(y)) {
         df <- bi.sum.cat(x, y, rnd, na.rm)
@@ -110,13 +110,13 @@ isum <- function(x, y = NULL, by = NULL, data = NULL, rnd = 1, na.rm = TRUE,
       } else {
         message('y is not specified as factor or as number.')
       }
-    }, 
+    },
     strata = {
-      if (na.rm) lvl <- as.character(unique(na.omit(by))) else 
+      if (na.rm) lvl <- as.character(unique(na.omit(by))) else
         lvl <- as.character(unique(by))
       lvl <- ifelse(is.na(lvl), "<NA>", lvl)
       df.lvl <- unlist(lapply(lvl, function(z) paste(c(x.name, y.name), z, sep = '_')))
-      
+
       if (is.factor(y) | is.character(y) | is.logical(y)) {
         df <- str.sum.cat(x, y, by, rnd, na.rm)
         for (i in 1:length(lvl)) row.names(df[[lvl[i]]])[1] <- as.character(x.name)
@@ -130,23 +130,23 @@ isum <- function(x, y = NULL, by = NULL, data = NULL, rnd = 1, na.rm = TRUE,
         strata <- df
       } else {
         message('y is not specified as factor or as number.')
-      } 
+      }
     }
   )
-  
+
   if (plot.show) {
-    main <- ifelse(is.null(y), paste0('Plot: ', x.name), 
-                   ifelse(is.null(by), 
-                          paste0('Plot: ', x.name, ' ~ ', y.name), 
-                          paste0('Plot: ', x.name, ' ~ ', y.name, 
+    main <- ifelse(is.null(y), paste0('Plot: ', x.name),
+                   ifelse(is.null(by),
+                          paste0('Plot: ', x.name, ' ~ ', y.name),
+                          paste0('Plot: ', x.name, ' ~ ', y.name,
                                  ' | ', by.name)))
     plot(
       iboxplot(x, y, by, na.rm = na.rm, main = main, xlab = x.name, ylab = y.name,
                legend.text = by.name)
     )
   }
-  
-  return(res) 
+
+  return(res)
 }
 
 uni.sum <- function(x, rnd = 1, na.rm = TRUE) {
@@ -165,24 +165,24 @@ uni.sum <- function(x, rnd = 1, na.rm = TRUE) {
   pvalue <- ifelse(pvalue < 0.001, "< 0.001", round(pvalue, 3))
   df <- data.frame(Obs. = len, NA. = na, Mean = v[1], Std.Dev = v[2],
                    Median = v[5], Q_1 = v[4], Q_3 = v[6],
-                   Min = v[3], Max = v[7], Normality = pvalue, 
+                   Min = v[3], Max = v[7], Normality = pvalue,
                    stringsAsFactors = FALSE)
   return(df)
 }
 
 bi.sum.num <- function(x, y, rnd = 1, na.rm = TRUE) {
-  df <- rbind(uni.sum(x, rnd, na.rm), 
+  df <- rbind(uni.sum(x, rnd, na.rm),
               uni.sum(y, rnd, na.rm))
-  df <- data.frame(df, Corr = c(htest.pvalue(x, y, test = 'corr'), ''), 
+  df <- data.frame(df, Corr = c(htest.pvalue(x, y, test = 'corr'), ''),
                    stringsAsFactors = FALSE)
   return(df)
 }
 
 bi.sum.cat <- function(x, y, rnd = 1, na.rm = TRUE) {
-  if (na.rm) lvl <- as.character(unique(na.omit(y))) else 
+  if (na.rm) lvl <- as.character(unique(na.omit(y))) else
     lvl <- as.character(unique(y))
   df <- uni.sum(x, rnd, na.rm)
-  df.lvl <- do.call(rbind, lapply(lvl, function(z) 
+  df.lvl <- do.call(rbind, lapply(lvl, function(z)
     uni.sum(x[y == z], rnd, na.rm)))
   lvl <- ifelse(is.na(lvl), "<NA>", lvl)
   row.names(df.lvl) <- lvl
@@ -192,16 +192,16 @@ bi.sum.cat <- function(x, y, rnd = 1, na.rm = TRUE) {
               ci.Lwr = round(df$Mean - (1.96 * df$SE), rnd),
               ci.Upr = round(df$Mean + (1.96 * df$SE), rnd))
   df <- do.call(rbind, list(df[1,], "_" = "_", df[-1,]))
-  
+
   pvalue <- htest.num(x, y, na.rm)
   df <- data.frame(df, c(pvalue[3], rep("", nrow(df) - 1)))
   colnames(df)[12] <- pvalue[2]
   return(df)
 }
 
-str.sum.num <- function(x, y, by, rnd = 1, na.rm = TRUE) 
+str.sum.num <- function(x, y, by, rnd = 1, na.rm = TRUE)
 {
-  if (na.rm) lvl <- as.character(unique(na.omit(by))) else 
+  if (na.rm) lvl <- as.character(unique(na.omit(by))) else
     lvl <- as.character(unique(by))
   df <- bi.sum.num(x, y, rnd, na.rm)
   df.lvl <- do.call(rbind, lapply(lvl, function(z) {
@@ -212,84 +212,95 @@ str.sum.num <- function(x, y, by, rnd = 1, na.rm = TRUE)
   return(df)
 }
 
-str.sum.cat <- function(x, y, by, rnd = 1, na.rm = TRUE) 
+str.sum.cat <- function(x, y, by, rnd = 1, na.rm = TRUE)
 {
-  if (na.rm) lvl <- as.character(unique(na.omit(by))) else 
+  if (na.rm) lvl <- as.character(unique(na.omit(by))) else
     lvl <- as.character(unique(by))
-  df <- lapply(lvl, function(z) 
+  df <- lapply(lvl, function(z)
     bi.sum.cat(x[by == z], y[by == z], rnd, na.rm))
   df <- structure(df, names = lvl)
   return(df)
 }
 
-htest.pvalue <- function(x, y = NULL, test = 'spw') 
+htest.pvalue <- function(x, y = NULL, test = 'spw')
 {
   pvalue <- switch (test,
     spw = {tryCatch({
       suppressWarnings(shapiro.test(x)$p.value)
     }, error = function(err) {
       return(NA)
-    })}, 
+    })},
     corr = {tryCatch({
       suppressWarnings(cor(x, y, use = 'na.or.complete'))
     }, error = function(err) {
       return(NA)
-    })}, 
+    })},
     ttest = {tryCatch({
       suppressWarnings(t.test(x ~ y)$p.value)
     }, error = function(err) {
       return(NA)
-    })}, 
+    })},
     wilcox = tryCatch({
       suppressWarnings(wilcox.test(x ~ y)$p.value)
     }, error = function(err) {
-      return(NA)}), 
+      return(NA)}),
     kwallis = tryCatch({
       suppressWarnings(kruskal.test(x ~ y)$p.value)
     }, error = function(err) {
-      return(NA)}), 
+      return(NA)}),
     anova = tryCatch({
       suppressWarnings(summary(aov(x ~ y))[[1]][1,5])
     }, error = function(err) {
-      return(NA)}), 
+      return(NA)}),
     fisher = tryCatch({
       suppressWarnings(fisher.test(x ~ y, simulate.p.value = TRUE)$p.value)
     }, error = function(err) {
-      return(NA)}), 
+      return(NA)}),
     chisq = tryCatch({
       suppressWarnings(chisq.test(x ~ y)$p.value)
     }, error = function(err) {
       return(NA)})
-    
+
   )
   pvalue <- ifelse(pvalue < 0.001, "< 0.001", round(pvalue, 3))
   return(pvalue)
 }
 
 
-htest.num <- function(x, y, na.rm = TRUE) 
+htest.num <- function(x, y, na.rm = TRUE)
 {
-  if (na.rm) lvl <- as.character(unique(na.omit(y))) else 
+  if (na.rm) lvl <- as.character(unique(na.omit(y))) else
     lvl <- as.character(unique(y))
-  if (length(lvl) > 2) {
-    if (shapiro.test(x)$p.value < 0.05) {
-      pvalue <- htest.pvalue(x, y, test = 'kwallis')
-      pvalue.name <- 'K-Wallis test'
+  spw.5 <- tryCatch({
+    suppressWarnings(shapiro.test(x)$p.value < 0.05)
+  }, error = function(err) {
+    return(NA)})
+
+  if (!is.na(spw.5)) {
+    if (length(lvl) > 2) {
+      if (spw.5) {
+        pvalue <- htest.pvalue(x, y, test = 'kwallis')
+        pvalue.name <- 'K-Wallis test'
+      } else {
+        pvalue <- htest.pvalue(x, y, test = 'anova')
+        pvalue.name <- 'ANOVA test'
+      }
     } else {
-      pvalue <- htest.pvalue(x, y, test = 'anova')
-      pvalue.name <- 'ANOVA test'
+      if (spw.5) {
+        pvalue <- htest.pvalue(x, y, test = 'wilcox')
+        pvalue.name <- 'Wilcox test'
+      } else {
+        pvalue <- htest.pvalue(x, y, test = 'ttest')
+        pvalue.name <- 't-test'
+      }
     }
   } else {
-    if (shapiro.test(x)$p.value < 0.05) {
-      pvalue <- htest.pvalue(x, y, test = 'wilcox')
-      pvalue.name <- 'Wilcox test'
-    } else {
-      pvalue <- htest.pvalue(x, y, test = 'ttest')
-      pvalue.name <- 't-test'
-    }
-  } 
-  df <- c('htest' = paste0('p-value (', pvalue.name, '): ', pvalue), 
-          'htest.name' = pvalue.name, 
+    pvalue.name <- "None"
+    pvalue <- NA
+  }
+
+  df <- c('htest' = paste0('p-value (', pvalue.name, '): ', pvalue),
+          'htest.name' = pvalue.name,
           'p.value' = pvalue)
   return(df)
 }
