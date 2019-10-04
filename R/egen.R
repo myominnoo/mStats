767fc3b5e0d58a1cdd6631702bbec6f36490b30a
Myo Minn Oo <dr.myominnoo@gmail.com>
2019-10-04 11:30:01 +0700

    mStats Version 2.3.1
    fixed egen() function

--- R/egen.R
+++ R/egen.R
@@ -56,20 +56,20 @@
 egen <- function(data = NULL, old.var, cut = NULL, lbl = NULL,
                  new.var = NULL, na.rm = FALSE)
 {
-  arguments <- as.list(match.call())
-  old.var.name <- unlist(arguments$old.var)
+    arguments <- as.list(match.call())
+    old.var.name <- unlist(arguments$old.var)
 
-  if (is.null(data)) {
-    if (!is.null(arguments$new.var))
-      stop("... no need to specify 'new.var' ...")
-    data <- as.numeric()
-  } else {
-    if (!is.data.frame(data)) {
-      stop("... specify a dataframe or NULL in the fist argument ...")
+    if (is.null(data)) {
+        if (!is.null(arguments$new.var))
+            stop("... no need to specify 'new.var' ...")
+        data <- as.numeric()
+    } else {
+        if (!is.data.frame(data)) {
+            stop("... specify a dataframe or NULL in the fist argument ...")
+        }
+        data <- data.frame()
     }
-    data <- data.frame()
-  }
-  UseMethod("egen", data)
+    UseMethod("egen", data)
 }
 
 
@@ -78,73 +78,73 @@ egen <- function(data = NULL, old.var, cut = NULL, lbl = NULL,
 egen.default <- function(data = NULL, old.var, cut = NULL, lbl = NULL,
                          new.var = NULL, na.rm = FALSE)
 {
-  stop("... Wrong data type ...")
+    stop("... Wrong data type ...")
 }
 
 egen.numeric <- function(data = NULL, old.var, cut = NULL, lbl = NULL,
                          new.var = NULL, na.rm = FALSE)
 {
-  arguments <- as.list(match.call())
-  old.var.name <- arguments$old.var
-  if (na.rm) old.var <- old.var[!is.na(old.var)]
+    arguments <- as.list(match.call())
+    old.var.name <- arguments$old.var
+    if (na.rm) old.var <- old.var[!is.na(old.var)]
 
-  if (is.null(cut)) {
-    cut <- 10
-    old.var.brk <- seq(min(old.var, na.rm = TRUE),
-                       max(old.var, na.rm = TRUE),
-                       cut)
-    old.var.brk <- c(old.var.brk[1],
-                     old.var.brk[2:(length(old.var.brk)-1)] - 1,
-                     old.var.brk[length(old.var.brk)] - 1)
-  }  else {
-    if (length(cut) == 1) {
-      old.var.brk <- seq(min(old.var, na.rm = TRUE),
-                         max(old.var, na.rm = TRUE),
-                         cut)
-      old.var.brk <- c(old.var.brk[1],
-                       old.var.brk[2:(length(old.var.brk)-1)] - 1,
-                       old.var.brk[length(old.var.brk)] - 1)
-    } else {
-      old.var.brk <- cut
+    if (is.null(cut)) {
+        cut <- 10
+        old.var.brk <- seq(min(old.var, na.rm = TRUE),
+                           max(old.var, na.rm = TRUE),
+                           cut)
+        old.var.brk <- c(old.var.brk[1],
+                         old.var.brk[2:(length(old.var.brk)-1)] - 1,
+                         old.var.brk[length(old.var.brk)] - 1)
+    }  else {
+        if (length(cut) == 1) {
+            old.var.brk <- seq(min(old.var, na.rm = TRUE),
+                               max(old.var, na.rm = TRUE),
+                               cut)
+            old.var.brk <- c(old.var.brk[1],
+                             old.var.brk[2:(length(old.var.brk)-1)] - 1,
+                             old.var.brk[length(old.var.brk)] - 1)
+        } else {
+            old.var.brk <- cut
+        }
     }
-  }
 
-  old.var.min <- min(old.var, na.rm = TRUE)
-  old.var.brk.min <- old.var.brk[1]
-  if (old.var.min < old.var.brk.min) {
-    old.var.brk <- c(old.var.min, old.var.brk)
-  }
-  if (old.var.brk.min < old.var.min) {
-    old.var.brk <- c(old.var.min, old.var.brk)
-    old.var.brk <- old.var.brk[old.var.brk >= old.var.min]
-  }
-  old.var.max <- max(old.var, na.rm = TRUE)
-  old.var.brk.max <- old.var.brk[length(old.var.brk)]
+    old.var.min <- min(old.var, na.rm = TRUE)
+    old.var.brk.min <- old.var.brk[1]
+    if (old.var.min < old.var.brk.min) {
+        old.var.brk <- c(old.var.min, old.var.brk)
+    }
+    if (old.var.brk.min < old.var.min) {
+        old.var.brk <- c(old.var.min, old.var.brk)
+        old.var.brk <- old.var.brk[old.var.brk >= old.var.min]
+    }
+    old.var.max <- max(old.var, na.rm = TRUE)
+    old.var.brk.max <- old.var.brk[length(old.var.brk)]
 
-  if (old.var.brk.max < old.var.max) {
-    old.var.brk <- c(old.var.brk, old.var.max)
-  } else {
-    old.var.brk <- old.var.brk[old.var.brk < old.var.max]
-    if (old.var.max != max(old.var.brk, na.rm = TRUE))
-      old.var.brk <- c(old.var.brk, old.var.max)
-  }
+    if (old.var.brk.max < old.var.max) {
+        old.var.brk <- c(old.var.brk, old.var.max)
+    } else {
+        old.var.brk <- old.var.brk[old.var.brk < old.var.max]
+        if (old.var.max != max(old.var.brk, na.rm = TRUE))
+            old.var.brk <- c(old.var.brk, old.var.max)
+    }
 
-  old.var.brk.len <- length(old.var.brk)
-  if (is.null(lbl)) {
-    old.var.lbl.lwr <- c(old.var.brk[1],
-                         old.var.brk[-c(1, old.var.brk.len)])
-    old.var.lbl.upr <- c(old.var.brk[2:(old.var.brk.len - 1)] - 1 ,
-                         old.var.brk[old.var.brk.len])
-    old.var.lbl <- paste0("l.",
-                          paste(old.var.lbl.lwr, old.var.lbl.upr, sep = "-"))
-  } else {old.var.lbl <- lbl}
+    old.var.brk.len <- length(old.var.brk)
+    if (is.null(lbl)) {
+        old.var.lbl.lwr <- c(old.var.brk[1],
+                             old.var.brk[-c(1, old.var.brk.len)])
+        old.var.lbl.upr <- c(old.var.brk[2:(old.var.brk.len - 1)] - 1 ,
+                             old.var.brk[old.var.brk.len])
+        old.var.lbl <- paste0("l.",
+                              paste(old.var.lbl.lwr, old.var.lbl.upr, sep = "-"))
+    } else {old.var.lbl <- lbl}
 
-  old.var <- cut(old.var, breaks = old.var.brk, labels = old.var.lbl, right = FALSE,
-                 include.lowest = TRUE)
+    old.var <- cut(old.var, breaks = old.var.brk, labels = old.var.lbl, right = FALSE,
+                   include.lowest = TRUE)
 
-  printMsg(paste0(length(old.var), " values generated with labels: ",
-                  paste0(old.var.lbl, collapse = ", ")))
-  return(old.var)
+    printMsg(paste0(length(old.var), " values generated with labels: ",
+                    paste0(old.var.lbl, collapse = ", ")))
+    return(old.var)
 }
 
 
@@ -153,22 +153,24 @@ egen.numeric <- function(data = NULL, old.var, cut = NULL, lbl = NULL,
 egen.data.frame <- function(data = NULL, old.var, cut = NULL, lbl = NULL,
                             new.var = NULL, na.rm = FALSE)
 {
-  arguments <- as.list(match.call())
-  data.name <- arguments$data
-  old.var.name <- gsub(" ", "", arguments$old.var)
-  old.var <- data[, old.var.name]
-  new.var.name <- as.character(arguments$new.var)
-  if (is.null(new.var.name))
-    new.var.name <- paste0(old.var.name, ".cat")
-  if (any(names(data) %in% new.var.name))
-    stop(paste0("... ", new.var.name, " already exists. Specify a new name ..."))
-  new.var <- egen.numeric(NULL, old.var, cut = cut, lbl = lbl, na.rm = na.rm)
-  data <- cbind(data, new.var)
-  names(data)[length(data)] <- new.var.name
+    arguments <- as.list(match.call())
+    data.name <- arguments$data
+    old.var.name <- gsub(" ", "", arguments$old.var)
+    old.var <- data[, old.var.name]
+    new.var.name <- as.character(arguments$new.var)
+    if (length(new.var.name) == 0)
+        new.var.name <- paste0(old.var.name, ".cat")
+    if (any(names(data) %in% new.var.name))
+        stop(paste0("... ", new.var.name, " already exists. Specify a new name ..."))
+    new.var <- egen.numeric(NULL, old.var, cut = cut, lbl = lbl, na.rm = na.rm)
+    data <- cbind(data, new.var)
+    names(data)[length(data)] <- new.var.name
 
-  printMsg(paste0("'", new.var.name,
-                  "' created & appended to dataframe '", data.name, "'"))
+    printMsg(paste0("'", new.var.name,
+                    "' created & appended to dataframe '", data.name, "'"))
 
-  return(data)
+    return(data)
 }
+
+
 
